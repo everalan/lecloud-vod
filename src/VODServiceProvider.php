e018f24479ed14091b1207ba79f62d85fc0260f0
Alan <everalan@everalan.com>
2020-08-28 23:58:24 +0800

    初始化

---
+++ src/VODServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Everalan\VOD;
+
+use Illuminate\Support\ServiceProvider;
+
+class VODServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+    }
+}
